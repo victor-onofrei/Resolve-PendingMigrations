2f8a3f68798834f63be1a2b74852d9782c201573
victor-onofrei <39416712+victor-onofrei@users.noreply.github.com>
2020-12-10 15:40:14 +0200

    Update pending_mig.ps1

--- pending_mig.ps1
+++ pending_mig.ps1
@@ -12,11 +12,11 @@ foreach ($user in $all_mailboxes)
         $archive_state = (Get-Mailbox -Identity $mailbox.Alias).ArchiveState
         if ($archive_state -eq "None") {
             Disable-Mailbox -Identity $mailbox.Alias -Confirm:$false
-            $routing_address = (Get-OrganizationConfig | Select -ExpandProperty MicrosoftExchangeRecipientEmailAddresses`
-            | ? {$_ -like "*mail.onmicrosoft.com"}).Split("@")[1]
+            $routing_address = (Get-OrganizationConfig | Select -ExpandProperty MicrosoftExchangeRecipientEmailAddresses | `
+             ? {$_ -like "*mail.onmicrosoft.com"}).Split("@")[1]
             Enable-RemoteMailbox $mailbox.Alias -RemoteRoutingAddress "$user@$routing_address"
-            Set-RemotMailbox $mailbox.UserPrincipalName -EmailAddresses $mailbox.EmailAddresses`
-            -EmailAddressPolicyEnabled $false
+            Set-RemoteMailbox $mailbox.UserPrincipalName -EmailAddresses $mailbox.EmailAddresses `
+             -EmailAddressPolicyEnabled $false
         } else {Write-Host "Mailbox" $user "has on-premise archive"}
     } else {Write-Host "Mailbox" $user "has on-premise content"}
 }
