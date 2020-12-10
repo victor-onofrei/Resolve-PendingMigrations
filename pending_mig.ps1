d888a591848ec7f84a31f015be36591e56ebe329
victor-onofrei <39416712+victor-onofrei@users.noreply.github.com>
2020-12-10 15:31:57 +0200

    Add files via upload

---
+++ pending_mig.ps1
@@ -0,0 +1,22 @@
+$input_path = "$env:homeshare\VDI-UserData\Download\generic\inputs"
+$output_path = "$env:homeshare\VDI-UserData\Download\generic\outputs\pending_mig"
+$file_name = "pending_mig.csv"
+$all_mailboxes = Get-Content $path\$file_name
+
+foreach ($user in $all_mailboxes)
+{
+    $item_count = Get-MailboxStatistics $user | select -ExpandProperty ItemCount
+    if ($item_count -le "300") {
+        $mailbox = Get-Mailbox -Identity $user -ResultSize Unlimited
+        $mailbox.EmailAddresses > $output_path\$user.txt
+        $archive_state = (Get-Mailbox -Identity $mailbox.Alias).ArchiveState
+        if ($archive_state -eq "None") {
+            Disable-Mailbox -Identity $mailbox.Alias -Confirm:$false
+            $routing_address = (Get-OrganizationConfig | Select -ExpandProperty MicrosoftExchangeRecipientEmailAddresses`
+            | ? {$_ -like "*mail.onmicrosoft.com"}).Split("@")[1]
+            Enable-RemoteMailbox $mailbox.Alias -RemoteRoutingAddress "$user@rwe.mail.onmicrosoft.com"
+            Set-RemotMailbox $mailbox.UserPrincipalName -EmailAddresses $mailbox.EmailAddresses`
+            -EmailAddressPolicyEnabled $false
+        } else {Write-Host "Mailbox" $user "has on-premise archive"}
+    } else {Write-Host "Mailbox" $user "has on-premise content"}
+}
